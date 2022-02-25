f028afafdfacaa9bbbaefd9803ac251beec012cd
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-02-25 20:47:01 -0300

    api_pokemon tsreact-config

---
+++ src/store/ducks/repositories/types.ts
@@ -0,0 +1,23 @@
+/*
+   ?Actions Types
+*/
+export enum RepositoriesTypes {
+   LOAD_REQUEST = '@repositories/LOAD_REQUEST',
+   LOAD_SUCCESS = '@repositories/LOAD_SUCCESS',
+   LOAD_FAILURE = '@repositories/LOAD_FAILURE'
+};
+/*
+   !Data Types
+*/
+export interface Repository {
+   id: number,
+   name: string
+}
+/*
+   fixme State Types
+*/
+export interface RepositoriesState {
+   readonly data: Repository[]
+   readonly loading: boolean
+   readonly error: boolean
+}

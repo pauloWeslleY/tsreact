78d0566c4113da2eceb5ea81a4f7011add2f4f30
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-02-27 19:23:40 -0300

    api_pokemon tsreact-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,52 @@
 module.exports = {
-   env: {
-      browser: true,
-      es6: true,
-      jest: true,
-   },
-   extends: [
-      'react-app',
-      'airbnb',
-      'plugin:@typescript-eslint/recommended',
-      'prettier/@typescript-eslint',
-   ],
-   globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-   },
-   parserOptions: {
-      ecmaFeatures: {
-         jsx: true,
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    "react-app",
+    "airbnb",
+    "prettier/prettier",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react", "import", "jsx-a11y"],
+  rules: {
+    "react/jsx-filename-extension": [
+      "error",
+      {
+        extensions: [".tsx"],
       },
-      ecmaVersion: 2018,
-      sourceType: 'module',
-   },
-   plugins: ['react', 'import', 'jsx-a11y'],
-   rules: {
-      'react/jsx-filename-extension': [
-         'error',
-         {
-            extensions: ['.tsx'],
-         },
-      ],
-      'import/prefer-default-export': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/explicit-member-accessibility': 'off'
-   },
-   settings: {
-      'import/parsers': {
-         '@typescript-eslint/parser': ['.ts', '.tsx'],
-      },
-      'import/resolver': {
-         typescript: {},
-      },
-   },
- };
+    ],
+    "import/prefer-default-export": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "import/extensions": "off",
+    "react/function-component-definition": "off",
+    "no-shadow": "off",
+    "default-param-last": "off",
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {},
+    },
+    "import/extensions": [".ts", ".tsx"],
+  },
+};
--- src/components/RepositoryList/index.tsx
+++ src/components/RepositoryList/index.tsx
@@ -1,11 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators, Dispatch } from "redux";
 
-export default class RepositoryList extends Component {
-   componentDidMount(){}
+import { Repository } from "../../store/ducks/repositories/types";
+import { ApplicationState } from "../../store";
 
-   render() {
-      return (
-         <ul />
-      )
-   }
+import * as RepositoriesAtions from "../../store/ducks/repositories/actions";
+
+interface StateProps {
+  repositories: Repository[];
 }
+
+interface DispatchProps {
+  loadRequest(): void;
+}
+
+type Props = StateProps & DispatchProps;
+class RepositoryList extends Component<Props> {
+  componentDidMount() {
+    const { loadRequest } = this.props;
+
+    loadRequest();
+  }
+
+  render() {
+    const { repositories } = this.props;
+    return <ul>{repositories.map((repository) => repository.name)}</ul>;
+  }
+}
+
+const mapStateToProps = (state: ApplicationState) => ({
+  repositories: state.repositories.data,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) =>
+  bindActionCreators(RepositoriesAtions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(RepositoryList);
